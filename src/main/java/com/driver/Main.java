c4dbb69174935af18f32d2dea85773c8122d998a
Neelam Dubey <ashidubey8208@gmail.com>
2023-05-13 18:19:31 +0530

    Finally Commit

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -2,5 +2,7 @@ package com.driver;
 
 public class Main {
     Product p=new Product();
-    p.product(67,87);
+    int n=p.product(67,89);
+    int m=p.product(56,47,89);
+    double o=p.product(76.54,85.98);
 }
