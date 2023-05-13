ed354fe2a58c4262ad768251c1535d5d48f41441
Neelam Dubey <ashidubey8208@gmail.com>
2023-05-13 22:35:09 +0530

    Assignment done

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -5,11 +5,8 @@ import java.util.Scanner;
 public class Main {
     Product p=new Product();
     Scanner s=new Scanner(System.in);
-    int x=s.nextInt(),y=s.nextInt(),z=s.nextInt();
-    int k=s.nextInt(), l=s.nextInt();
-    double t=s.nextDouble(), q=s.nextDouble();
-    int n=p.product(x,y);
-    int m=p.product(k,l,z);
-    double o=p.product(t,q);
+    int x=p.product(s.nextInt(),s.nextInt());
+    int y=p.product(s.nextInt(),s.nextInt(),s.nextInt());
+    double z=p.product(s.nextDouble(),s.nextDouble());
 
 }
